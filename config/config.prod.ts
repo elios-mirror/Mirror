7a5c68801867f359e234f712da52c4bba891ba22
Léo <leohub@live.fr>
2019-11-14 14:02:15 +0100

    Add prod config

---
+++ config/config.prod.ts
@@ -0,0 +1,14 @@
+
+
+export default {
+    api: {
+        address: 'https://api.elios-mirror.com',
+        client_id: 1,
+        client_secret: 'Rp52CEoYWjiIA0kRTTGspdbjee3tQxSaNCVn7J87'
+    },
+    sockets: {
+        address: 'http://sockets.elios-mirror.com',
+        port: '4224'
+    },
+    model: 'LKD28376382'
+}
