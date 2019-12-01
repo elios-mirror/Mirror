32f6a5ea3a57817dc77ebda5430622acfcf871f8
Léo <leohub@live.fr>
2019-12-01 23:47:48 +0100

    Revert config file

--- config/config.ts
+++ config/config.ts
@@ -2,12 +2,12 @@
 
 export default {
     api: {
-        address: 'https://admin.test',
+        address: 'https://dev.elios-mirror.com',
         client_id: 1,
         client_secret: 'Rp52CEoYWjiIA0kRTTGspdbjee3tQxSaNCVn7J87'
     },
     sockets: {
-        address: 'http://localhost',
+        address: 'http://sockets.elios-mirror.com',
         port: '4224'
     },
     model: 'LKD28376382'
